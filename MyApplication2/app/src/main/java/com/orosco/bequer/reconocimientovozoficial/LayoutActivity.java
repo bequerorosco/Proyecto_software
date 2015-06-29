438b50b3d7b4216f11cb69e59004f6287d2e2bd6
bequerorosco <ilegal1.1@hotmail.com>
2015-06-29 12:59:21 -0500

    Login inicio

---
+++ MyApplication2/app/src/main/java/com/orosco/bequer/reconocimientovozoficial/LayoutActivity.java
@@ -0,0 +1,17 @@
+package com.orosco.bequer.reconocimientovozoficial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by user on 29/06/2015.
+ */
+public class LayoutActivity extends Activity {
+
+    @Override
+    public void onCreate (Bundle saveInstance){
+    super.onCreate(saveInstance);
+    setContentView(R.layout.table_layout);
+
+    }
+}
