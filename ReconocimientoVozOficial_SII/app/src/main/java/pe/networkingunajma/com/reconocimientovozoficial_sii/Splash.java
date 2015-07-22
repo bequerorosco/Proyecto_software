8cbba19fef3a2fe146b1d741c3e4eae4f7d291b6
bequerorosco <ilegal1.1@hotmail.com>
2015-07-22 15:38:03 -0500

    Imagen bienvenida !!

---
+++ ReconocimientoVozOficial_SII/app/src/main/java/pe/networkingunajma/com/reconocimientovozoficial_sii/Splash.java
@@ -0,0 +1,51 @@
+package pe.networkingunajma.com.reconocimientovozoficial_sii;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+
+
+
+/**
+ * Created by user on 22/07/2015.
+ */
+public class Splash extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashh);
+
+        final ImageView iv=(ImageView) findViewById(R.id.imageView);
+        final Animation an= AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
+        final Animation an2= AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
+
+        iv.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                iv.startAnimation(an2);
+                setContentView(R.layout.table_layout);
+                Intent i=new Intent(getBaseContext(),MainActivity.class);
+                startActivity(i);
+
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+    }
+
+}
